fcc9cc6873013da7c53e8c969dd95dbfdf11804b
Amro145 <amroaltayeb14@gmail.com>
2026-02-23 01:17:59 +0200

    feat: Expand CORS origins to include localhost:3000 and localhost:5173.

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ ConnectToDb().catch(err => {
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-    origin: "http://localhost:3001",
+    origin: ["http://localhost:3000", "http://localhost:3001", "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }))
