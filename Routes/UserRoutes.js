1bf20e6bd2dc0eb836e1197ea26ce057884deb53
Amro145 <amroaltayeb14@gmail.com>
2026-02-23 03:45:17 +0200

    fix: Change /logout route from GET to POST.

--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
-router.get("/logout", logout);
+router.post("/logout", logout);
 router.get("/me", protect, getMe);
 
 export default router;
